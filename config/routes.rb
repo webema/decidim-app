7ccc62aff6626154e1c04ceb317621035a917010
Heiner Sameisky <hei.sam@gmail.com>
2022-07-25 14:29:57 +0200

    Mount sidekiq web interface

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
+  require 'sidekiq/web'
+
+  authenticate :user, lambda { |u| u.admin? } do # TODO: should be system admin only
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   mount Decidim::Core::Engine => '/'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
