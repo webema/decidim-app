69a6e2bf2f44e5f7f8ee6e5dfc90f628ec07f843
Heiner Sameisky <hei.sam@gmail.com>
2022-10-01 10:31:26 +0200

    Store session in redis

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -119,6 +119,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+
+  config.session_store :redis_store
 end
 
 Mail.register_interceptor(
