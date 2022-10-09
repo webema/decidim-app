4b36bf795d81b7ae599f6d3dc192380dfe3b49f5
Heiner Sameisky <hei.sam@gmail.com>
2022-10-09 17:36:11 +0200

    Fix links with missing protocol in quill editor

--- app/packs/src/decidim/editor.js
+++ app/packs/src/decidim/editor.js
@@ -6,6 +6,37 @@ import "src/decidim/vendor/image-upload.min"
 
 import htmlEditButton from "quill-html-edit-button";
 
+// START fix links with missing protocol
+
+const Link = Quill.import('formats/link');
+
+// Override the existing property on the Quill global object and add custom protocols
+Link.PROTOCOL_WHITELIST = ['http', 'https', 'mailto'];
+
+class CustomLinkSanitizer extends Link {
+  static sanitize(url) {
+  // Run default sanitize method from Quill
+  const sanitizedUrl = super.sanitize(url);
+
+  // Not whitelisted URL based on protocol so, let's return `blank`
+  if (!sanitizedUrl || sanitizedUrl === 'about:blank') return sanitizedUrl;
+
+  // Verify if the URL already have a whitelisted protocol
+  const hasWhitelistedProtocol = this.PROTOCOL_WHITELIST.some(function(protocol) {
+      return sanitizedUrl.startsWith(protocol);
+  });
+
+  if (hasWhitelistedProtocol) return sanitizedUrl;
+
+  // if not, then append only 'https' to not to be a relative URL
+  return `https://${sanitizedUrl}`;
+  }
+}
+
+Quill.register(CustomLinkSanitizer, true)
+
+// END fix links with missing protocol
+
 Quill.register({
   "modules/htmlEditButton": htmlEditButton
 })
