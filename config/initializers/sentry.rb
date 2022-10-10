212b0b41c02ec01b616efbf49877963486175b89
Heiner Sameisky <hei.sam@gmail.com>
2022-10-10 13:30:20 +0200

    Update decidim-ideas

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,7 +2,7 @@ Sentry.init do |config|
   config.dsn = 'https://dcdc76ed57444f40aacd8f380339b3b9@o97645.ingest.sentry.io/6781597'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  config.environment = 'production'
+  config.enabled_environments = %w[staging production]
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
